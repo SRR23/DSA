a6b1858195b1fdb3ac6bc20fc720f4fa64777e1c
SRR23 <srr267@gmail.com>
2023-02-21 11:24:31 +0600

    added

---
+++ Heap/PriorityQueueUsingHeap.cpp
@@ -0,0 +1,150 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class MaxHeap
+{
+public:
+    vector<int> nodes;
+
+    MaxHeap()
+    {
+    }
+
+    void up_heapify(int indx)
+    {
+        while (indx > 0 && nodes[indx] > nodes[(indx - 1) / 2])
+        {
+            swap(nodes[indx], nodes[(indx - 1) / 2]);
+            indx = (indx - 1) / 2;
+        }
+    }
+    void Insert(int value)
+    {
+        nodes.push_back(value);
+        up_heapify(nodes.size() - 1);
+    }
+
+    void Print_Heap()
+    {
+        for (int i = 0; i < nodes.size(); i++)
+        {
+            cout << nodes[i] << " ";
+        }
+        cout << '\n';
+    }
+
+    void down_heapify(int indx)
+    {
+        while (1)
+        {
+            int largest = indx;
+            int l = 2 * indx + 1;
+            int r = 2 * indx + 2;
+            if (l < nodes.size() && nodes[largest] < nodes[l])
+            {
+                largest = l;
+            }
+            if (r < nodes.size() && nodes[largest] < nodes[r])
+            {
+                largest = r;
+            }
+            if (largest == indx)
+            {
+                break;
+            }
+            swap(nodes[indx], nodes[largest]);
+            indx = largest;
+        }
+    }
+
+    void Delete(int indx)
+    {
+        if (indx >= nodes.size())
+        {
+            return;
+        }
+        swap(nodes[indx], nodes[nodes.size() - 1]);
+        nodes.pop_back();
+        down_heapify(indx);
+    }
+
+    int getMax()
+    {
+        if (nodes.empty())
+        {
+            cout << "Heap is empty\n";
+            return -1;
+        }
+        return nodes[0];
+    }
+
+    int ExtractMax()
+    {
+        if (nodes.empty())
+        {
+            cout << "Heap is empty\n";
+            return -1;
+        }
+        int m = nodes[0];
+        Delete(0);
+        return m;
+    }
+
+    void build_from_array(vector<int> &a)
+    {
+        nodes = a;
+        int n = nodes.size();
+        int last_non_leaf = n / 2 - 1;
+        for (int i = last_non_leaf; i >= 0; i--)
+        {
+            down_heapify(i);
+        }
+    }
+
+    int getSize()
+    {
+        return nodes.size();
+    }
+};
+
+class PriorityQueue
+{
+public:
+    MaxHeap heap;
+    PriorityQueue()
+    {
+    }
+
+    void push(int x)
+    {
+        heap.Insert(x);
+    }
+    void pop()
+    {
+        heap.Delete(0);
+    }
+    int top()
+    {
+        return heap.getMax();
+    }
+    int size()
+    {
+        return heap.getSize();
+    }
+};
+
+int main()
+{
+    PriorityQueue pq;
+    pq.push(5);
+    pq.push(7);
+    pq.push(10);
+    pq.push(1);
+    pq.push(2);
+
+    while (pq.size() != 0)
+    {
+        cout << pq.top() << " ";
+        pq.pop();
+    }
+}
