eb7640ec4e8e8a0173dfbaa1d04a0c47ae7760f5
SRR23 <srr267@gmail.com>
2023-04-11 21:58:28 +0600

    added

---
+++ DFS/CSES_LongestFlightRoute.cpp
@@ -0,0 +1,62 @@
+#include <bits/stdc++.h>
+using namespace std;
+const int N = 1e5 + 10;
+
+int n, m, a, b, in[100005], dis[100005], pre[100005];
+vector<int> v[100005], ans;
+
+int main()
+{
+    cin >> n >> m;
+    for (int i = 0; i < m; i++)
+    {
+        cin >> a >> b;
+        v[a].push_back(b);
+        in[b]++;
+    }
+    for (int i = 1; i <= n; i++)
+    {
+        dis[i] = -1e9;
+    }
+    queue<int> q;
+    dis[1] = 0;
+    for (int i = 1; i <= n; i++)
+    {
+        if (in[i] == 0)
+        {
+            q.push(i);
+        }
+    }
+    while (!q.empty())
+    {
+        int now = q.front();
+        q.pop();
+        for (int i : v[now])
+        {
+            if (dis[i] < dis[now] + 1)
+            {
+                pre[i] = now;
+                dis[i] = dis[now] + 1;
+            }
+            in[i]--;
+            if (in[i] == 0)
+                q.push(i);
+        }
+    }
+    if (dis[n] < 0)
+        cout << "IMPOSSIBLE\n";
+    else
+    {
+        ans.push_back(n);
+        while (ans.back() != 1)
+        {
+            ans.push_back(pre[ans.back()]);
+        }
+        reverse(ans.begin(), ans.end());
+        cout << ans.size() << "\n";
+        for (int i : ans)
+        {
+            cout << i << " ";
+        }
+    }
+}
