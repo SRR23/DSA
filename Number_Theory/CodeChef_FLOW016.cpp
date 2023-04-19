34f6bd8df9edfc44ac13de8db41b48d707889119
SRR23 <srr267@gmail.com>
2023-04-19 14:59:10 +0600

    added

---
+++ Number_Theory/CodeChef_FLOW016.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int a, b;
+        cin >> a >> b;
+        int GCD = __gcd(a, b);
+        ll LCM = (1LL * a * b) / GCD;
+        cout << GCD << " " << LCM << '\n';
+    }
+}
